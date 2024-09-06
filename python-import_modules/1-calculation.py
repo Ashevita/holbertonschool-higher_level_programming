4162cc478353025b9a226af19b1b2b0a83827358
Ashevita <9362@holbertonstudents.com>
2024-09-06 11:02:22 +0200

    add

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
-    print(f"{a} - {b} = {result}")
-    print(f"{a} * {b} = {result}")
-    print(f"{a} / {b} = {result}")
+    print("{} + {} = {}".format(a, b, result))
+    print("{} - {} = {}".format(a, b, result))
+    print("{} * {} = {}".format(a, b, result))
+    print("{} / {} = {}".format(a, b, result))
