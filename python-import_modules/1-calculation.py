013c993270263a0df346ea5b15fc1992d1799cd2
Ashevita <9362@holbertonstudents.com>
2024-09-06 10:59:08 +0200

    add

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add
+    a = 10
+    b = 5
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
+    print(f"{a} - {b} = {result}")
+    print(f"{a} * {b} = {result}")
+    print(f"{a} / {b} = {result}")
