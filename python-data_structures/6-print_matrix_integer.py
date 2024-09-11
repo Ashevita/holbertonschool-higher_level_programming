8c4f47880513a8adb2e3930f3b28d9bf8ffebbee
Ashevita <9362@holbertonstudents.com>
2024-09-11 09:47:35 +0200

    erreur

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print(matrix[i][j], end=' ')
-        print()
+    for row in matrix:
+        for i in range(len(row)):
+            if i < len(row) - 1:
+                print("{:d}".format(row[i]), end=" ")
+            else:
+                print("{:d}".format(row[i]))
 
