f6e7c63f04a4690b8659a0ae763b4f8cd14ac27b
Ashevita <9362@holbertonstudents.com>
2024-09-11 09:40:20 +0200

    ajout fichier

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+            return
+    if idx not in range(1, 5):
+            return
+    else:
+           return(my_list[idx])
