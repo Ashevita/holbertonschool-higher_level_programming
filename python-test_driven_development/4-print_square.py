78a9da94a230bcf6f56108f92a2574cc2e7dab5d
Ashevita <9362@holbertonstudents.com>
2024-09-13 16:10:32 +0200

    add files

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This is a module for matrix operations.
+It contains a function to divide all elements of a matrix.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    Returns:
+        None
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
