769b5de3a0ea1b4d9c9ffc4da573db62ff3e5975
Ashevita <9362@holbertonstudents.com>
2024-09-13 15:54:25 +0200

    add files

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Module qui contient une fonction pour afficher un nom.
+"""
 def say_my_name(first_name, last_name=""):
     """
     Prints 'My name is <first name> <last name>'
