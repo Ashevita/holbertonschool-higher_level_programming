0ecd3a61587071cbc419cb4aef9ca98668ac8add
Ashevita <9362@holbertonstudents.com>
2024-09-19 09:34:20 +0200

    rectification

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """Module to create class
+This module defines an empty class called `Square`.
 """
 
-
+# Définition de la classe Square
 class Square:
+    """Empty class that defines a square.
+    
+    This class doesn't currently have any properties, methods, or attributes.
+    It is essentially a placeholder for future development.
+    """
     pass
