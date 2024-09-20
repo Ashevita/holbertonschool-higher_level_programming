5dd9cb69fdbeb25ccbacd5be6a27644b7f88fd0f
Ashevita <9362@holbertonstudents.com>
2024-09-20 20:22:29 +0200

    recti

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""Module to create class
+This module defines an empty class called `Rectangle`.
+"""
+
+
 class Rectangle:
+    """Empty class that defines a Rectangle.
+
+    This class doesn't currently have any properties, methods, or attributes.
+    It is essentially a placeholder for future development.
+    """
     pass
