6af68a6f68ace6a237bbd0ef30de54e851233968
Ashevita <9362@holbertonstudents.com>
2024-09-27 22:52:38 +0200

    dragon

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""Classes Fish, Bird et FlyingFish."""
+
+
+class SwimMixin:
+    """Mixin that provides swimming ability."""
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Mixin that provides flying ability."""
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """A dragon that can swim, fly, and roar."""
+    def roar(self):
+        """Method for dragon's roar."""
+        print("The dragon roars!")
