25dffbcc4d2b6d85d2cd156a011d42f2e40e9c8a
Ashevita <9362@holbertonstudents.com>
2024-10-04 21:07:09 +0200

    add file for correction

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,15 +1,21 @@
+#!/usr/bin/python3
+"""Module to create class
+"""
+
+
 class Student:
     """Defines a student by first_name, last_name, and age."""
 
     def __init__(self, first_name, last_name, age):
+
         """Initializes a Student instance."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instance.
-        
         If attrs is a list of strings, only those attributes will be
         retrieved. Otherwise, all attributes are retrieved.
         """
