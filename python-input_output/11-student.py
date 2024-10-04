00f8d385ae7731563c97a514a1e7e2dfac3f9320
Ashevita <9362@holbertonstudents.com>
2024-10-04 21:05:40 +0200

    add file for correction

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,44 +1,28 @@
-#!/usr/bin/python3
-"""Module to create class
-"""
-
-
 class Student:
-
-    """Class that defines a student by first_name, last_name, and age."""
+    """Defines a student by first_name, last_name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the student instance
-        with first_name, last_name, and age."""
+        """Initializes a Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of the Student instance.
-        If attrs is a list of strings, only those attributes are returned.
-        Otherwise, all attributes are returned.
+        """Retrieves a dictionary representation of a Student instance.
+        
+        If attrs is a list of strings, only those attributes will be
+        retrieved. Otherwise, all attributes are retrieved.
         """
         if isinstance(attrs, list):
             if all(isinstance(attr, str) for attr in attrs):
-                # Filter the attributes based on the given list
-                result = {}
-                for attr in attrs:
-                    if hasattr(self, attr):
-                        result[attr] = getattr(self, attr)
-                return result
-        else:
-            # Return all attributes if no filter is applied
-            return self.__dict__
+                return {
+                    key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)
+                }
+        return self.__dict__
 
-
-def reload_from_json(self, json):
-
-    """Replaces all attributes of the Student
-        instance using the given json dictionary.
-        You can assume json will always be a
-        dictionary with keys matching attribute names.
-        """
-    for key, value in json.items():
-        # Replace or add the attributes from the json dictionary
-        setattr(self, key, value)
+    def reload_from_json(self, json):
+        """Replaces all attributes of
+        the Student instance from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
