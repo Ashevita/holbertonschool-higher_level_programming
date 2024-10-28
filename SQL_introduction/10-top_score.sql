339c3c1fd54f21a6d471d8601cae3da769225ace
Ashevita <9362@holbertonstudents.com>
2024-10-28 15:27:22 +0100

    SQL

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- script for display in order
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- script for display in order
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
