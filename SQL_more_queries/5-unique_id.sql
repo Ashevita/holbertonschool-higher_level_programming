527bd4eb91cfb0149f819925078ccdcaba58f5ca
Ashevita <9362@holbertonstudents.com>
2024-10-31 15:20:29 +0100

    SQL

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- script for creates a table
+CREATE TABLE IF NOT EXISTS unique_id (
+		id INT DEFAULT 1 UNIQUE,
+		name VARCHAR(256)
+);
---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- script for create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- script for create table
+CREATE TABLE IF NOT EXISTS states (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(256) NOT NULL
+);
