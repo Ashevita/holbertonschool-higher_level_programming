f67e59ea610379e9fcf1be3b656e6fcecd80b959
Ashevita <9362@holbertonstudents.com>
2024-10-31 15:24:48 +0100

    SQL

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- script for create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Utilise la base de données hbtn_0d_usa
+USE hbtn_0d_usa;
+
 -- script for create table
 CREATE TABLE IF NOT EXISTS states (
 		id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
